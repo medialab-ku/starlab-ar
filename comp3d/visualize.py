3daf3882671c5d52f0ab7d380e97f4c9adca8867
salguanz <salguanz@naver.com>
2020-08-24 17:50:48 +0900

    visualize code to check outputs

---
+++ comp3d/visualize.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import h5py
+import numpy as np
+import open3d as o3d
+
+h5path = sys.argv[1]
+print (h5path)
+h5file = h5py.File(h5path, 'r')
+
+for key1 in h5file.keys():
+    col = h5file[key1]
+    for key2 in col.keys():
+        pts = np.array(h5file[key1][key2])
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(pts)
+        o3d.visualization.draw_geometries([pcd])
