194779edf8266ce185fd04f86f1ac906f2558bd5
salguanz <salguanz@naver.com>
2020-08-24 17:54:26 +0900

    debug vis

--- comp3d/visualize.py
+++ comp3d/visualize.py
@@ -9,7 +9,7 @@ h5file = h5py.File(h5path, 'r')
 for key1 in h5file.keys():
     col = h5file[key1]
     for key2 in col.keys():
-        pts = np.array(h5file[key1][key2])
+        pts = np.array(h5file[key1][key2]).squeeze(0)
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(pts)
         o3d.visualization.draw_geometries([pcd])
