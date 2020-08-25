343201e90e6545705064610c8b74b5500877a307
miru3137 <miru3137@korea.ac.kr>
2020-08-25 14:40:48 +0900

    Load labels and colors

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
+using namespace cv::dnn;
 
 int main()
 {
@@ -19,6 +21,30 @@ int main()
 	// close all windows
 	waitKey(0);
 	destroyAllWindows();
+
+	// read labels
+	vector<string> labels;
+	{
+		ifstream ifs("mscoco_labels.txt");
+		string line;
+		while (getline(ifs, line))
+			labels.push_back(line);
+	}
+
+	// read colors
+	vector<Scalar> colors;
+	{
+		ifstream ifs("instance_colors.txt");
+		string line;
+		while (getline(ifs, line)) {
+			stringstream ss(line);
+			int r, g, b;
+			ss >> r;
+			ss >> g;
+			ss >> b;
+			colors.push_back(Scalar(r, g, b, 255));
+		}
+	}
 
 	// exit program
 	return EXIT_SUCCESS;
