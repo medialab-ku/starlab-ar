0d30306cbcce245160a7d2ee0776bddf19bf2e7e
miru3137 <miru3137@korea.ac.kr>
2020-08-25 14:37:31 +0900

    Test OpenCV

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <cstdlib>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
+	// print test message
 	cout << "Hello World!" << endl;
 
+	// read test image
+	const auto image = imread("test.bmp");
+
+	// show test image
+	imshow("Test Image", image);
+
+	// close all windows
+	waitKey(0);
+	destroyAllWindows();
+
+	// exit program
 	return EXIT_SUCCESS;
 }
