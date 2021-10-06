c6cf5f15247614ccac8463b02acdaf2bf28c8ed8
Jaehyung Doh <knuppe22@gmail.com>
2021-10-06 12:20:53 +0900

    Added comments to explain parameter values

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ for directory in [DIR_IMAGE, DIR_VIDEO, DIR_MESH]:
 ############################## Parameter Setting ##############################
 
 
+# TUM RGB-D
+# DATA_NAME = 'rgbd_dataset_freiburg1_xyz'
+# MAP_SIZE = (640, 480)
+# CAM_INTRINSIC = (591.1, 590.1, 331.0, 234.0)
+# PYRAMID_LEVEL = 3
+
+# RealSense D455 (color to depth align)
+# DATA_NAME = 'rs2'
+# MAP_SIZE = (1280, 720)
+# CAM_INTRINSIC = (642.2863159179688, 642.2863159179688, 635.0969848632812, 360.14208984375)
+# PYRAMID_LEVEL = 3
+
+# RealSense D455 (depth to color align)
 DATA_NAME = 'rs2'
 MAP_SIZE = (1280, 800)
 CAM_INTRINSIC = (637.4338989257812, 636.5624389648438, 637.0335693359375, 410.9555358886719)
