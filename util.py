fb3cef655bae403d0a6b1e8ae192be08fdaddb0b
Jaehyung Doh <knuppe22@gmail.com>
2021-10-24 15:33:29 +0900

    normal map i/o function

--- util.py
+++ util.py
@@ -8,8 +8,22 @@ def show_depth_map(name: str, map: np.ndarray) -> None:
     cv2.imshow(name, image)
     cv2.waitKey(1)
 
+def show_normal_map(name: str, map: np.ndarray) -> None:
+    image = 255 * (map * 0.5 + 0.5)
+    image = image.astype(np.uint8)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imshow(name, image)
+    cv2.waitKey(1)
+
 def write_depth_map(path: str, map: np.ndarray) -> None:
     image = 255 * (map / 4)
     image = image.astype(np.uint8)
+    cv2.imwrite(path, image)
+    cv2.waitKey(1)
+
+def write_normal_map(path: str, map: np.ndarray) -> None:
+    image = 255 * (map * 0.5 + 0.5)
+    image = image.astype(np.uint8)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     cv2.imwrite(path, image)
     cv2.waitKey(1)
